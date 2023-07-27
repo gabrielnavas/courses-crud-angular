9c84ac16a35291da1fd73e2c27ff744c8c352184
navas <gabrielnavas@email.com>
2023-07-27 18:50:13 -0300

    add course component and configure course route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // if acess root of the application, render this
+  {
+  path: '', 
+  pathMatch: 'full',
+  redirectTo: 'courses'
+}, {
+  path: 'courses',
+  loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
